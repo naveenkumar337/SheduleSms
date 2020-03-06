fbd83eab4f4adb4c71fb943d9207ac582b84e240
ravali.yarram <ravali.yarram@DIT-129>
2020-03-06 18:59:31 +0530

    changes in registration page

--- RavaliScheduledSMS/js/Registration.js
+++ RavaliScheduledSMS/js/Registration.js
@@ -1,62 +1,10 @@
-﻿
-//    if ($('#username').val() == "") {
-//        $.jGrowl("please Enter username")
-//        return false;
-//    }
-//    else if ($('#txtmail').val() == "") {
-//        $.jGrowl("please Enter Email")
-//        return false;
-//    }
-//    else if ($('#contactno').val() == "") {
-//        $.jGrowl("please enter contactNO")
-//        return false;
-//    }
-//    else if ($('#password').val() == "") {
-//        $.jGrowl("please enter Numeric password")
-//        return false;
-//    }
-//    else if ($('#cfmpswd').val() == "") {
-//        $.jGrowl("please enter Confirmpassword")
-//        return false;
-//    }
-//    if ($('#password').val() != $('#cfmpswd').val()) {
-//        $.jGrowl("password and confirmpassword must be match")
-//        return false;
-//    }
-//})
-//if ($('#username').val() != "" && $('#txtmail').val() != "" && $('#contactno').val() != "" && $('#password').val() != "" && $('#cfmpswd').val() !== "") {
-//    var request = $.ajax({
-//        url: 'http://localhost:58919/api/Registraion/Registarion_ADD',
-//        method: 'POST',
-//        data: obj,
-
-//        success: (function (obj) {
-//            debugger;
-//            alert("Registered successfully" + obj.username);
-//        })
-//    })
-//    error: (function () {
-//        alert("error");
-//    })
-//}
-
-
-//$('.input').keypress(function (e) {
-//    if (e.which == 13) {
-//        $('form#submit').submit();
-//        return false;
-//    }
-//});
-//})
-
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $('#submit').click(function (event) {
         var obj = {};
         obj.username = $('#username').val(),
         obj.email = $('#txtmail').val(),
         obj.contactNo = $('#contactno').val(),
-        obj.password = $('#password').val(),
+        obj.Password = $('#password').val(),
         obj.ConfirmPassword = $('#cfmpswd').val()
         if ($('#username').val() == "") {
             $.jGrowl("please Enter username")
@@ -82,22 +30,26 @@ $(document).ready(function () {
             $.jGrowl("password and confirmpassword must be match")
             return false;
         }
-        if (obj.username != "" && obj.email != "" && obj.contactNo != "" && obj.password != "" && obj.ConfirmPassword) {
+        if (obj.username != "" && obj.email != "" && obj.contactNo != "" && obj.Password != "" && obj.ConfirmPassword) {
             debugger;
             var request = $.ajax({
                 url: 'http://localhost:58919/api/Registraion/Registarion_ADD',
                 method: 'POST',
                 data: obj,
 
-            }).then(function (response) {
+            }).then(function (obj) {
+               
                 debugger;
-                if (response == '-1') {
-                    alert("Authentication failed");
+                if (obj.objresponse.ResponseCode == '101') {
+                    $.jGrowl("User alredy existed please registerd with new user");
                 }
                 else {
-                    sessionStorage.setItem("username", response.UserName);
+                                    
+                    sessionStorage.setItem("username", obj.objresponse.UserName);
                     debugger;
+                    $.jGrowl("User Registration Completed Successfully")
                     location.href = "Login%20.aspx";
+                    return false;   
                 }
             });
         }
